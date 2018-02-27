afb93cb7938a10a2a64357cb55d6baa317410ebe
Michael Grogan <firstclassanalyticsmg@gmail.com>
2018-02-27 20:56:09 +0000

    Create functions.R

---
+++ functions.R
@@ -0,0 +1,7 @@
+laspeyres <- function(price1period1,price1period2,quantity1period1,quantity1period2,price2period1,quantity2period2,price2period2,quantity2period1) {((quantity1period1*price1period2)+(quantity2period1*price2period2))/((quantity1period1*price1period1)+(quantity2period1*price2period1))}
+paasche <- function(price1period1,price1period2,quantity1period1,quantity1period2,price2period1,quantity2period2,price2period2) {((quantity1period2*price1period2)+(quantity2period2*price2period2))/((quantity1period2*price1period1)+(quantity2period2*price2period1))}
+fisher <- function(paasche,laspeyres) {sqrt(paasche*laspeyres)}
+gdpdeflator <-function(nominalgdp,realgdp) {(nominalgdp/realgdp)}
+moneymultiplier <- function(reserveratio) {(1/reserveratio)}
+ruleof70 <- function(x) {(70/x)}
+moneysupply <- function(velocity,avgprice,quantity) {((avgprice*quantity)/(velocity))}
