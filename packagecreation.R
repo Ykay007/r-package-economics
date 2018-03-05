f6972af6dc481bebb4a8a060cfa0baceb8769cc6
Michael Grogan <firstclassanalyticsmg@gmail.com>
2018-03-05 20:59:45 +0000

    Update packagecreation.R

--- packagecreation.R
+++ packagecreation.R
@@ -12,8 +12,8 @@ install("economics")
 library(economics)
 
 #RATIO CALCULATIONS
-laspeyres=laspeyres(3,4,50,70,4.4,60,4.5,70)
-paasche=paasche(3,4,50,70,4.4,60,4.5)
+laspeyres=laspeyres(2,5,60,75,5.6,60,4.5,75)
+paasche=paasche(2,5,60,75,5.6,60,4.5)
 fisher(paasche,laspeyres)
 moneymultiplier(0.7)
 ruleof70(1.5)
